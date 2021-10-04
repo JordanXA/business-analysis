9691533d2c533f5c886fb8baf6225588538d5ec3
JordanXA <JordanXA@users.noreply.github.com>
2021-10-04 16:43:12 +0000

    static'

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,28 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return render_template('index.html')
+
+@app.route('/buy')
+def buy():
+    return render_template('buy.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
 
